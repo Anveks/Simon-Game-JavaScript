b64f643a4b6a6b9d26ea5f63a706a0164483b0ba
Anveks <vekselmana@gmail.com>
2024-02-13 12:01:48 +0200

    check score broken not fixed yet

--- script.js
+++ script.js
@@ -1,78 +1,115 @@
+// COLOR BLOCK CLASS
+class ColorBlock {
+  constructor(app) {
+    this.app = app;
+    this.isActive = false;
+    this.element = document.createElement("div");
+    this.element.classList.add("color-block");
+    this.element.addEventListener("click", this.handleClick.bind(this));
+  }
 
+  handleClick() {
+    const index = this.app.colorBlocks.indexOf(this);
+    this.app.playersSequence.push(index);
+    this.toggleActive();
+
+    if (this.app.playersSequence.length === this.app.colorsNumber) {
+      console.log(this.app.playersSequence);
+      console.log(this.app.computerSequence);
+
+      this.app.checkSequence(this.app.playersSequence, this.app.computerSequence);
+    }
+  }
+
+  toggleActive() {
+    this.element.classList.toggle("active");
+    setTimeout(() => this.element.classList.toggle("active"), 1000);
+  }
+}
+
+// APP CLASS
 class App {
-
   computerSequence = [];
   playersSequence = [];
-  colorsNumber = document.querySelector(".diff").value;
+  difficulty = document.querySelector(".diff");
+  colorsNumber;
   startBtn = document.querySelector(".btn-start");
+  score = document.querySelector(".score");
+  turn = document.querySelector(".turn");
 
-  constructor(){
+  constructor() {
     this.colorBlocks = [];
     this.gameCont = document.querySelector(".game-cont");
 
     this.startBtn.addEventListener('click', this.startGame.bind(this));
     this.renderColorBlocks();
-  };
 
-  renderColorBlocks(){
+    this.difficulty.addEventListener('change', this.renderColorBlocks.bind(this));
+  }
+
+  renderColorBlocks() {
+    this.gameCont.innerHTML = "";
+    this.colorsNumber = this.difficulty.value;
+    console.log(this.colorsNumber);
+    
     for (let i = 0; i < this.colorsNumber; i++) {
-      const colorBlock = new ColorBlock();
+      const colorBlock = new ColorBlock(this);
       this.gameCont.insertAdjacentElement("beforeend", colorBlock.element);
       this.colorBlocks.push(colorBlock);
-    };
-  };
+    }
+  }
 
   startGame() {
     this.computersTurn();
-  };
+  }
 
   computersTurn() {
-    this.computerSequence = []; // clear the sequence arr
-    this.toggleInterface();
+    this.turn.innerHTML = "🤖 Computer's turn."
+    this.toggleTurn();
+
+    this.computerSequence = [];
+    this.playersSequence = []; // Clear player's sequence
+    this.toggleInterface(); // deactivate the interface while it is computer's turn
 
     for (let i = 0; i < this.colorsNumber; i++) {
-      const randomNum = Math.floor(Math.random() * 4);
+      const randomNum = Math.floor(Math.random() * this.colorsNumber);
       setTimeout(() => {
         this.colorBlocks[randomNum].toggleActive();
-        console.log('this is from the first set timeout');
-      }, i * 1200); // delay each block's activation by i seconds
+      }, i * 1200);
       this.computerSequence.push(randomNum); 
     }
 
-    setTimeout(() => this.toggleInterface(), 1200 * this.colorsNumber); // the interface is enabled again after all the code in the loop finishes
-  };
+    setTimeout(() => {
+      this.toggleInterface();
+      this.turn.innerHTML = "🙂 Player's turn."
+      this.toggleTurn();
+      console.log(this.computerSequence);
+    }, 1200 * this.colorsNumber);
+  }
 
   toggleInterface() {
     this.gameCont.classList.toggle("unclickable");
   }
 
-  playersTurn() {
-    // players turn logic
-  };
-
-  checkSequence() {
-    // check the sequence
+  toggleTurn() {
+    this.turn.classList.add('bump');
+    this.turn.classList.remove('bump');
   }
 
-}
-
-class ColorBlock {
-  constructor(){
-    this.isActive = false; // Whether the block is active or not
-    this.element = document.createElement("div"); // The HTML element representing the block
-    this.element.classList.add("color-block");
-    this.element.addEventListener("click", this.toggleActive.bind(this)); // Bind click event handler
+  checkSequence(player, computer) {
+    if (player.toString() === computer.toString()) {
+      this.turn.innerHTML = "Correct! 🎉"
+      this.score.innerHTML = +this.score.innerHTML + 1;
+      setTimeout(() => this.computersTurn(), 2000);
+    } else {
+      this.turn.innerHTML = "Better luck next time! ❌";
+      this.score.innerHTML = 0;
+    }
   }
-
-  toggleActive() {
-    console.log('clicked');
-    this.element.classList.toggle("active");
-    setTimeout(() =>  this.element.classList.toggle("active"), 1000);
-  }
-
 }
 
 const app = new App();
+
 
 // let timeout = colorsNumber; // set the timeot stop the same as the color blocks number
 
